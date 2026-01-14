1132e587c543785db70167e94991405dd68ddb99
Rahul-R79 <rahul.devworks@gmail.com>
2026-01-14 22:59:05 +0530

    chore: initial next.js project configuration completed

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center">
+      <h1 className="text-3xl font-bold">Portfolio Website</h1>
+    </div>
+  );
+}
